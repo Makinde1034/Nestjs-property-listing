96277696bdb64309e3792b57882d22b869e32fb3
YhomiAce <kareemyomi91@gmail.com>
2024-04-16 15:49:14 +0100

    feat(schema): Kareem-WASD-55- Schema setup

---
+++ src/common/types/user.type.ts
@@ -0,0 +1,1 @@
+export type UserProfileType = 'individual' | 'company';
--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,6 +1,9 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { PrimaryGeneratedColumn } from 'typeorm';
 
+@ObjectType()
 export default class BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn('uuid')
   id: string;
 }
