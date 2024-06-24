24878323b5b11da4ad6f2626f77c27df20b249b1
Ehis Fidelis <meelisfidelis@gmail.com>
2024-06-24 14:47:44 +0100

    Revert "feat(listing): Ehis-WASD-194 edit migration for  ability to flag listing"
    This reverts commit def178e3f6f782fdcdbaaebaa92fe634b34e6b83.

---
+++ src/database/migrations/1718792136004-flagListing.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2024, Waseet LLC. All rights reserved.
+ * For license. See license.txt
+ */
+
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class FlagListing1718792136004 implements MigrationInterface {
+  name = 'FlagListing1718792136004';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "flag_listing" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "listingId" uuid NOT NULL, "inappropriate" boolean NOT NULL, "falseInformation" boolean NOT NULL, "userId" character varying NOT NULL, "description" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cd39e70214a4ef1c651a8dc604f" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "listing" ADD "isListingFlagged" boolean NOT NULL DEFAULT false`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "listing" ADD "disableListing" boolean NOT NULL DEFAULT false`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "flag_listing" ADD CONSTRAINT "FK_902a609e89ed9af1cdd73056e24" FOREIGN KEY ("listingId") REFERENCES "listing"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "flag_listing" DROP CONSTRAINT "FK_902a609e89ed9af1cdd73056e24"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "listing" DROP COLUMN "disableListing"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "listing" DROP COLUMN "isListingFlagged"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`,
+    );
+    await queryRunner.query(`DROP TABLE "flag_listing"`);
+    await queryRunner.query(
+      `CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
+}
