79a9548314c5a796e1535171197a1179e0a765bc
Ehis Fidelis <meelisfidelis@gmail.com>
2024-07-29 07:45:28 +0100

    Task/Ehis-WASD-413-backend-rerwork-listingtype update listing update listing type input

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,7 @@ import { PaymentModule } from './modules/payment/payment.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { JobService } from './modules/in-app-services/job.scheduler';
 import { WebHookModule } from './modules/webhook/web-hook.module';
-import { DynamicEnumService } from './modules/app/services/dynamic-enum.services';
+
 import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
@@ -98,6 +98,6 @@ import { AdminModule } from './modules/admin/admin.module';
     WebHookModule,
   ],
   controllers: [],
-  providers: [AppResolver, JobService, DynamicEnumService],
+  providers: [AppResolver, JobService],
 })
 export class AppModule {}
