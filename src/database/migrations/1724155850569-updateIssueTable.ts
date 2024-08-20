5882d76a3f7a1e14986d6cb095ddeb2bcaa83e39
Ehis Fidelis <meelisfidelis@gmail.com>
2024-08-20 13:51:00 +0100

    Task/Ehis-WASD-update-listing ads ability to delete attribute and attributeSet Icon

--- src/database/migrations/1724155850569-updateIssueTable.ts
+++ src/database/migrations/1724155850569-updateIssueTable.ts
@@ -1,36 +1,84 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+/*
+ * Copyright (c) 2024, Waseet LLC. All rights reserved.
+ * For license. See license.txt
+ */
+
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UpdateIssueTable1724155850569 implements MigrationInterface {
-    name = 'UpdateIssueTable1724155850569'
+  name = 'UpdateIssueTable1724155850569';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "issue_category" ADD "deletedAt" TIMESTAMP`);
-        await queryRunner.query(`ALTER TABLE "auction" ADD "deletedAt" TIMESTAMP`);
-        await queryRunner.query(`CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `);
-        await queryRunner.query(`CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "issue_category" ADD "deletedAt" TIMESTAMP`,
+    );
+    await queryRunner.query(`ALTER TABLE "auction" ADD "deletedAt" TIMESTAMP`);
+    await queryRunner.query(
+      `CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`);
-        await queryRunner.query(`ALTER TABLE "auction" DROP COLUMN "deletedAt"`);
-        await queryRunner.query(`ALTER TABLE "issue_category" DROP COLUMN "deletedAt"`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`);
-        await queryRunner.query(`CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `);
-        await queryRunner.query(`CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`,
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`,
+    );
+    await queryRunner.query(`ALTER TABLE "auction" DROP COLUMN "deletedAt"`);
+    await queryRunner.query(
+      `ALTER TABLE "issue_category" DROP COLUMN "deletedAt"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
 }
--- src/modules/listing/controllers/listing-type.controller.ts
+++ src/modules/listing/controllers/listing-type.controller.ts
@@ -5,6 +5,7 @@
 
 import {
   Controller,
+  Delete,
   Post,
   Query,
   UploadedFile,
@@ -58,5 +59,17 @@ export class ListingTypeController {
       listingTypeId,
       icon,
     );
+  }
+
+  @Delete('delete-attributeSet-icon')
+  @UseGuards(RestAccessTokenGuard)
+  async deleteAttributeSetIcon(@Query('attributeId') attributeSetId: string) {
+    return await this.attributeService.deleteAttributeIcon(attributeSetId);
+  }
+
+  @Delete('delete-attribute-icon')
+  @UseGuards(RestAccessTokenGuard)
+  async deleteAttributeIcon(@Query('attributeId') attributeId: string) {
+    return await this.attributeService.deleteAttributeSetIcon(attributeId);
   }
 }
