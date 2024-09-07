3f44f23446ca4032780ef432e6ede2d3aa62cf40
Ehis Fidelis <meelisfidelis@gmail.com>
2024-09-07 09:36:02 +0100

    Task/WASD-customer-and-employee-refactor-and-update update role

---
+++ src/database/migrations/1725698140250-addUserToFlaglistingTable.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddUserToFlaglistingTable1725698140250 implements MigrationInterface {
+    name = 'AddUserToFlaglistingTable1725698140250'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" DROP COLUMN "parentIssue"`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" DROP COLUMN "childIssue"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP COLUMN "use"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD "use" boolean DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" ADD "parentIssueId" uuid`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" ADD "childIssueId" uuid`);
+        await queryRunner.query(`CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" ADD CONSTRAINT "FK_d64b86859cdfb82d7c70a202e95" FOREIGN KEY ("parentIssueId") REFERENCES "parent_issue"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" ADD CONSTRAINT "FK_a7eef2fd8bf7db084416fb89c98" FOREIGN KEY ("childIssueId") REFERENCES "child_issue"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "flag_listing" DROP CONSTRAINT "FK_a7eef2fd8bf7db084416fb89c98"`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" DROP CONSTRAINT "FK_d64b86859cdfb82d7c70a202e95"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_bfbc9e263d4cea6d7a8c9eb3ad"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_b36cb2e04bc353ca4ede00d87b"`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" DROP COLUMN "childIssueId"`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" DROP COLUMN "parentIssueId"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP COLUMN "use"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" DROP COLUMN "approve"`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD "use" boolean DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD "approve" boolean DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" ADD "childIssue" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "flag_listing" ADD "parentIssue" character varying`);
+        await queryRunner.query(`CREATE INDEX "IDX_bfbc9e263d4cea6d7a8c9eb3ad" ON "role_permissions_permission" ("permissionId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_b36cb2e04bc353ca4ede00d87b" ON "role_permissions_permission" ("roleId") `);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_b36cb2e04bc353ca4ede00d87b9" FOREIGN KEY ("roleId") REFERENCES "role"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "role_permissions_permission" ADD CONSTRAINT "FK_bfbc9e263d4cea6d7a8c9eb3ad2" FOREIGN KEY ("permissionId") REFERENCES "permission"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+}
