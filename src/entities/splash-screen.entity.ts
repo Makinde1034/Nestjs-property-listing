5adda954977b573eb91255cc6c622931c5699aae
Ehis Fidelis <meelisfidelis@gmail.com>
2024-09-16 12:08:23 +0100

    update user ticket status

---
+++ src/entities/splash-screen.entity.ts
@@ -0,0 +1,7 @@
+// import { Entity, PrimaryGeneratedColumn } from "typeorm";
+// import { SplashScreen } from "../modules/admin/services/splash-screen.service";
+
+// @Entity()
+// export class SplashScreen{
+//     @PrimaryGeneratedColumn()
+// }
---
+++ src/modules/admin/services/splash-screen.service.ts
@@ -0,0 +1,3 @@
+// export class SplashScreen {
+//   constructor(private);
+// }
