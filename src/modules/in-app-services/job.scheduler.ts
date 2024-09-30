291fcdc352c6b060bdb36a0ed976b64d7f816d73
Ehis Fidelis <meelisfidelis@gmail.com>
2024-09-30 13:54:38 +0100

    add cron job to update promoted listing, featured listing and offer  status

--- src/modules/in-app-services/job.scheduler.ts
+++ src/modules/in-app-services/job.scheduler.ts
@@ -31,9 +31,7 @@ export class JobService {
   ) {}
   logger = new Logger(JobService.name);
 
-  // @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, { timeZone: 'Africa/Cairo' })
-
-  @Cron(CronExpression.EVERY_30_SECONDS, { timeZone: 'Africa/Cairo' }) //TODO: remove after testing
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, { timeZone: 'Africa/Cairo' })
   async handleCron() {
     await this.sendAlertOnIncompleteOffers();
     await this.sendNotificationForNewListingBasedOnSearchHistory();
