42d6cb066cd8669a96e14db86e985bc04973edd7
Ehis Fidelis <meelisfidelis@gmail.com>
2024-10-18 15:06:36 +0100

    edit config file

--- src/modules/payment/service-providers/nafath.service.ts
+++ src/modules/payment/service-providers/nafath.service.ts
@@ -1,28 +1,29 @@
 import { HttpService } from '@nestjs/axios';
 import { Logger } from '@nestjs/common';
-import { ConfigService } from 'aws-sdk';
 import { AxiosRequestConfig } from 'axios';
 import {
   HyperpayConfig,
   getHyperpayConfigName,
 } from '../../../config/payment/hyper-payment.config';
+import { NafathConfig } from '../../../config/auth/nafath.config';
+import { ConfigService } from '@nestjs/config';
 
 export class NafathService {
   private readonly logger = new Logger(NafathService.name);
-  private hyperPayConfig: HyperpayConfig;
+  private nafathConfig: NafathConfig;
   private readonly options: AxiosRequestConfig;
 
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
   ) {
-    this.hyperPayConfig = this.configService.get<HyperpayConfig>(
+    this.nafathConfig = this.configService.get<HyperpayConfig>(
       getHyperpayConfigName(),
     );
 
     this.options = {
       headers: {
-        Authorization: `Bearer ${this.hyperPayConfig.token}`,
+        Authorization: `Bearer ${this.nafathConfig.token}`,
         'Content-Type': 'application/x-www-form-urlencoded',
       },
     };
