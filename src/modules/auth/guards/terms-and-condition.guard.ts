53ed3f57cd1f35c6ef54d052e360a5f9279638f7
Ehis Fidelis <meelisfidelis@gmail.com>
2024-11-08 18:30:32 +0100

    admin workflow

--- src/modules/auth/guards/terms-and-condition.guard.ts
+++ src/modules/auth/guards/terms-and-condition.guard.ts
@@ -16,7 +16,7 @@ import { IS_PUBLIC_KEY } from '../decorators/permision.decorator';
 import { Reflector } from '@nestjs/core';
 
 @Injectable()
-export class UserTwoGuard implements CanActivate {
+export class TermsAndConditionGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
