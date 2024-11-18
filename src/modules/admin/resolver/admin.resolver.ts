b437650f3456278e9d2ea906ed736472c942f0ab
Ehis Fidelis <meelisfidelis@gmail.com>
2024-11-18 17:17:45 +0100

    add resolver

--- src/modules/admin/resolver/admin.resolver.ts
+++ src/modules/admin/resolver/admin.resolver.ts
@@ -197,6 +197,7 @@ export class AdminResolver {
       ctx.req.user,
     );
   }
+
   @UseGuards(AdminGuard)
   @UseGuards(AccessTokenGuard, PermissionsGuard)
   @Permissions(PermissionsEnum.COUPONS_CHANGE_STATUS)
