89235ad63f9f605107e5dbc73142750d25224325
Ehis Fidelis <meelisfidelis@gmail.com>
2024-11-22 15:39:41 +0100

    adds ability to cancle, reject , apeal and confirm

---
+++ src/common/enums/service-provider.ts
@@ -0,0 +1,8 @@
+export enum ServiceProvidedStatus {
+  PENDING = 'pending',
+  ACCEPTED = 'accepted',
+  REJECTED = 'rejected',
+  APPEALED = 'appealed',
+  CANCELED = 'canceled',
+  COMPLETED = 'confirmed',
+}
