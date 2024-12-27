39223b7a4c5de0907e42c08227b06d2da3425ad7
Ehis Fidelis <meelisfidelis@gmail.com>
2024-12-27 11:52:47 +0100

    log web hook

--- src/modules/webhook/services/web-hook.services.ts
+++ src/modules/webhook/services/web-hook.services.ts
@@ -69,7 +69,7 @@ export class WebhookService {
       if (!secretFromConfiguration)
         throw new Error('Missing hyperPayDecriptionToken');
 
-      const httpBody = JSON.stringify(payload?.encryptedBody.encryptedBody); // Should be a hex string
+      const httpBody = JSON.stringify(payload?.encryptedBody?.encryptedBody); // Should be a hex string
       console.log(httpBody);
 
       // // Convert hex strings to binary buffers
