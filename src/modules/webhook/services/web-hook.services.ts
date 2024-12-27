5cd441acd73c76257e13532a315bd49689fbb912
Ehis Fidelis <meelisfidelis@gmail.com>
2024-12-27 12:51:20 +0100

    log web hook

--- src/modules/webhook/services/web-hook.services.ts
+++ src/modules/webhook/services/web-hook.services.ts
@@ -55,7 +55,6 @@ export class WebhookService {
     authTagFromHttpHeader: string,
   ) {
     try {
-      console.log(payload);
       const algorithm = 'aes-256-gcm';
       const secretFromConfiguration =
         this.hyperPayConfig.hyperPayDecriptionToken;
@@ -69,33 +68,34 @@ export class WebhookService {
       if (!secretFromConfiguration)
         throw new Error('Missing hyperPayDecriptionToken');
 
-      const httpBody = JSON.stringify(payload?.encryptedBody?.encryptedBody); // Should be a hex string
-      console.log(httpBody);
+      // const httpBody = JSON.stringify(payload?.encryptedBody); // Should be a hex string
 
       // // Convert hex strings to binary buffers
       const key = Buffer.from(secretFromConfiguration, 'hex');
       const iv = Buffer.from(ivfromHttpHeader, 'hex');
       const authTag = Buffer.from(authTagFromHttpHeader, 'hex');
-      const cipherText = Buffer.from(httpBody, 'hex');
+      const cipherText = Buffer.from(payload?.encryptedBody, 'hex');
 
-      // // Log converted values for debugging
-      console.log('Key:', key);
-      console.log('IV:', iv);
-      console.log('AuthTag:', authTag);
-      console.log('CipherText:', cipherText);
+      // console.log(cipherText);
+
+      // // // Log converted values for debugging
+      // // console.log('Key:', key);
+      // // console.log('IV:', iv);
+      // // console.log('AuthTag:', authTag);
+      // // console.log('CipherText:', cipherText);
 
       // // Decrypt the data
-      // const decipher = crypto.createDecipheriv(algorithm, key, iv);
-      // decipher.setAuthTag(authTag);
+      const decipher = crypto.createDecipheriv(algorithm, key, iv);
+      decipher.setAuthTag(authTag);
 
-      // const decrypted = Buffer.concat([
-      //   decipher.update(cipherText),
-      //   decipher.final(),
-      // ]).toString('utf8');
+      const decrypted = Buffer.concat([
+        decipher.update(cipherText),
+        decipher.final(),
+      ]).toString('utf8');
 
-      // const data: WebHookPaymentResponse = JSON.parse(decrypted);
-      // console.log('Decrypted data:', data);
-      // await this.paymentService.finalizeTransaction(data);
+      const data: WebHookPaymentResponse = JSON.parse(decrypted);
+      console.log('Decrypted data:', data);
+      await this.paymentService.finalizeTransaction(data);
 
       return new SuccessResponse();
     } catch (error) {
