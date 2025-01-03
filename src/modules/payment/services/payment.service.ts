64d2a4b4e2546982369865335b82fca65e4e2099
Ehis Fidelis <meelisfidelis@gmail.com>
2025-01-03 11:48:20 +0100

    update bit bucket pipeline

--- src/modules/payment/services/payment.service.ts
+++ src/modules/payment/services/payment.service.ts
@@ -204,7 +204,6 @@ export class PaymentService {
       this.logger.log(error);
     }
   }
-
   async refundPayment(createPaymentInput: RefundPaymentData) {
     const checkout =
       await this.hyperPayService.refundPayment(createPaymentInput);
